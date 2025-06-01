9c2b43375d863b37ecff369722f66afea597a9de
Aditya Singh <singhas1418@gmail.com>
2025-06-01 11:50:08 +0530

    removing extra space form the Auth controller

--- Backend/Controllers/Auth.js
+++ Backend/Controllers/Auth.js
@@ -8,229 +8,170 @@ const bcrypt = require("bcrypt")
 
 
 // send otp function 
-    
-async function sendOtp(req ,res){
+async function sendOtp(req, res) {
     var otp;
-    try{
-
-         const {email} = req.body;
-
-    // perform some validation like email must not empty 
-
-    if(!email){
-        return res.status(500).json({
-            sucess:false,
-            message:"Please fill the email "
-        })
-    }
-
-
-    // let's check user is already exist or not 
-
-
-    try{
-
-        const existingUser = await User.findOne({email})
-        if(!existingUser){
-            // generaotr otp 
-         otp = generateOTP.generate(6 ,{
-            upperCaseAlphabets:false,
-            lowerCaseAlphabets:false,
-            specialChars:false,
-         })
-
-         console.log("OTP generated" , otp)
-       // chek OTO is unique or not 
-
-       const uniqueOTP = await OTP.findOne({otp : otp})
-
-       while(uniqueOTP){
-           otp = generateOTP.generate(6 ,{
-            upperCaseAlphabets:false,
-            lowerCaseAlphabets:false,
-            specialChars:false,
-         })
-           uniqueOTP = await findOne({otp : otp})
-       }
-
-       // Make enty on the data base 
-
-
-       // create an Enty on DB 
-
-       const OTPBody = await OTP.create({
-        email,
-        otp
-       })
-       console.log(OTPBody)
-
-
-       return res.status(200).json({
-        sucess:true,
-        message:"The Otp is created sucessfully",
-        data:OTPBody,
-       })
-
-    //    send the email 
-        }     
-
-        else{
+    try {
+        const { email } = req.body;
+        // perform some validation like email must not empty 
+        if (!email) {
             return res.status(500).json({
-                sucess:false,
-                message:"User is All ready exixt please login whith your password "
+                sucess: false,
+                message: "Please fill the email "
             })
         }
-            
-    }
-
+        // let's check user is already exist or not 
+        try {
+            const existingUser = await User.findOne({ email })
+            if (!existingUser) {
+                // generaotr otp 
+                otp = generateOTP.generate(6, {
+                    upperCaseAlphabets: false,
+                    lowerCaseAlphabets: false,
+                    specialChars: false,
+                })
+                console.log("OTP generated", otp)
+                // chek OTO is unique or not 
+                const uniqueOTP = await OTP.findOne({ otp: otp })
+                // not a best apporach for creating an OTP you can use any API for this.
+                while (uniqueOTP) {
+                    otp = generateOTP.generate(6, {
+                        upperCaseAlphabets: false,
+                        lowerCaseAlphabets: false,
+                        specialChars: false,
+                    })
+                    uniqueOTP = await findOne({ otp: otp })
+                }
+                // Make enty on the data base
+                // create an Enty on DB 
+                const OTPBody = await OTP.create({
+                    email,
+                    otp
+                })
+                console.log(OTPBody)
+                return res.status(200).json({
+                    sucess: true,
+                    message: "The Otp is created sucessfully",
+                    data: OTPBody,
+                })
+                //    send the email adding in future 
+            }  else {
+                return res.status(500).json({
+                    sucess: false,
+                    message: "User is All ready exixt please login whith your password "
+                })}         
     
-
-    catch(error){
-        console.log("Getting error while make connnection with the data base " ,error)
-    }
-
-    }
-
-    catch(error){
+        } catch (error) {
+            console.log("Getting error while make connnection with the data base ", error)
+        }
+        // if try block not working properly then move on catch block       
+    }  catch (error) {
         console.log("getting  error while ")
     }
-   
-
-
 }
+//---------------------------------------------- OTP function Ends---------------------------------------------------------------------------------------
 
+//-----------------------------------------------SignUp function Start-----------------------------------------------------------------------------------
+async function signUp(req, res) {
 
-
-
-
-// signip
-
-async function signUp(req ,res){
-
-    try{
-
+    try {
         const {
             firstName,
-             lastName,
-              email,
-              password,
-              confrimPassword,
-              otp,
-              acconutType,
-              phoneNumber
-
-
+            lastName,
+            email,
+            password,
+            confrimPassword,
+            otp,
+            acconutType,
+            phoneNumber
         } = req.body;
 
         // validation
 
-        if(!firstName || !lastName || !email || !password ||  !confrimPassword || !otp || !acconutType || !phoneNumber){
-          return res.status(500).json({
-            suucess:false,
-            message:"Please Enter All the details",
-            // data:res
-          })
-
-          
-        }
-
-        // check is it existing user 
-
-        const isExistingUser = await User.findOne({email}) ;
-
-        if(isExistingUser){
-           
+        if (!firstName || !lastName || !email || !password || !confrimPassword || !otp || !acconutType || !phoneNumber) {
             return res.status(500).json({
-                sucess:false,
-                message:"This user is already exist Please login !"
+                suucess: false,
+                message: "Please Enter All the details",
+                // data:res
             })
         }
 
-        // check the OTP is matched or not 
+        // check is it existing user 
+        const isExistingUser = await User.findOne({ email });
+        if (isExistingUser) {
+            return res.status(500).json({
+                sucess: false,
+                message: "This user is already exist Please login !"
+            })
+        }
 
-        const otpChecked = await OTP.findOne({email});
+        // check the OTP is matched or not
+        const otpChecked = await OTP.findOne({ email });
+        if (!otpChecked) {
+            return res.status(500).json({
+                sucess: false,
+                message: "OTP is not matched"
+            })
+        }
 
-        if(!otpChecked ){
+        //Chek password does't matchs 
+        if(password ==!confrimPassword){
             return res.status(500).json({
                 sucess:false,
-                message:"OTP is not matched"
+                message:"Password and Confrim Password does Not Match"
             })
         }
 
         // hseh the password 
-        let hassingPassword ;
-       try{
-         hassingPassword =  await bcrypt.hash(password , 10);
+        let hassingPassword;
+        try {
+            hassingPassword = await bcrypt.hash(password, 10);
+            console.log("Your hash password is ", hassingPassword);
+            // ReadableStreamDefaultController  res.status(200).json({
+            //     sucess:true,
+            //     message:"The password is hashed sucessfully "
+            // })
 
-        console.log("Your hash password is ",hassingPassword);
+        }catch (error) {
+            console.log("Getting problem while hassing the password ")
+        }
 
-        // ReadableStreamDefaultController  res.status(200).json({
-        //     sucess:true,
-        //     message:"The password is hashed sucessfully "
-        // })
+        // make enty in the data base 
 
-       }
+        // create an enty for ref's
 
-       catch(error){
-        console.log("Getting problem while hassing the password ")
-       }
-        
-       // make enty in the data base 
+        //    const profileDetails = await Profile.create({
 
-       // create an enty for ref's
+        //     gender:null,
+        //     contactNumber:null,
+        //      address:null,
+        //      about:null,
 
-    //    const profileDetails = await Profile.create({
+        //    })
 
-    //     gender:null,
-    //     contactNumber:null,
-    //      address:null,
-    //      about:null,
-        
-    //    })
-
-       // Course ref
-
-
-    
-
-      const userData = await User.create({
-
-              firstName,
-              lastName,
-              email,
-              password : hassingPassword,
-              confrimPassword,
-              otp,
+        // Course ref
+        const userData = await User.create({
+            firstName,
+            lastName,
+            email,
+            password: hassingPassword,
+            confrimPassword,
+            otp,
             //   additionalDetails:profileDetails._id,
-              acconutType,
-              phoneNumber,
-              image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`
-
-       })   
-
-       console.log(userData)
-
-
-       return res.status(201).json({
-        sucess:true,
-        message:"You are login Sucessfully "
-       })
-
-
-        
-
-    }
-
-
-    catch(error){
-      
-        res.status(500).json({
-            sucess:false,
-            message:"Getting error while sign up"
+            acconutType,
+            phoneNumber,
+            image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`
         })
-
+        console.log(userData)
+        return res.status(201).json({
+            sucess: true,
+            message: "You are login Sucessfully "
+        })
+    } catch (error) {
+        res.status(500).json({
+            sucess: false,
+            message: "Getting error while sign up"
+        })
         console.log(error)
-
     }
 }
 
@@ -242,4 +183,4 @@ async function signUp(req ,res){
 
 // change password 
 
-module.exports=  {sendOtp , signUp} ;
+module.exports = { sendOtp, signUp };
